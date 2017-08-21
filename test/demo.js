dfdaa29b59e54d0c0b87fc1ea1f8ed7fe6372bb6
#云淡然 <cloudcome@163.com>
2017-08-21 11:33:00 +0800

    ~ demo

--- test/demo.js
+++ test/demo.js
@@ -11,13 +11,25 @@
 var console = require('../src/index');
 
 
-console.log('console', 'log');
-console.info('console', 'info');
-console.warn('console', 'warn');
-console.error('console', 'error');
-console.logWithTime('console', 'log', 'withTime');
-console.infoWithTime('console', 'info', 'withTime');
-console.warnWithTime('console', 'warn', 'withTime');
-console.errorWithTime('console', 'error', 'withTime');
+// console.log('console', 'log');
+// console.info('console', 'info');
+// console.warn('console', 'warn');
+// console.error('console', 'error');
+// console.logWithTime('console', 'log', 'withTime');
+// console.infoWithTime('console', 'info', 'withTime');
+// console.warnWithTime('console', 'warn', 'withTime');
+// console.errorWithTime('console', 'error', 'withTime');
 
+var points = ['-', '=', '>', '|', '<', '='];
+var times = 0;
+var time = setInterval(function () {
+    var index = times % (points.length - 1);
+    console.point(points[index]);
 
+    if (times === 30) {
+        clearInterval(time);
+        console.pointEnd();
+    }
+
+    times++;
+}, 300);
